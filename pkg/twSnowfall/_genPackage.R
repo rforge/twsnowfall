4b06eb94b827b5a51370f66fd9004b396fd07313
tomaschwutz <tomaschwutz@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-09-22 14:45:00 +0000

    deleting only man/Rd files on docu generation deleting the entire folder including dir .svn corrupts repository
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/twsnowfall@5 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/twSnowfall/_genPackage.R
+++ pkg/twSnowfall/_genPackage.R
@@ -25,7 +25,7 @@
 .tmp.f <- function(){
 	# generate documentation	
 	library(inlinedocs)
-	unlink( "man", recursive=TRUE)	# take care, entire man directory deleted
+	unlink( file.path("man","*.Rd") )	
 	package.skeleton.dx(".")
 	#file.copy( Sys.glob(file.path("inst","genData","*.Rd")), "man" )	# copy descriptions of data
 	#unlink(file.path("man","twDEMC.Rd"))   # else overwrites alias twDEMC to twDEMCInt 
