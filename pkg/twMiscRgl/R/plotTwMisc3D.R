afd996771fedc90595526fe8e8e36c692f2b2fba
tomaschwutz <tomaschwutz@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-11-27 20:52:58 +0000

    twApply3DMesh: change return type to list instead of array with attributes plot.twApply3DMesh: use attribute origRange to plot the full range of the original sample instead of the grid or subsample only
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/twsnowfall@21 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/twMiscRgl/R/plotTwMisc3D.R
+++ pkg/twMiscRgl/R/plotTwMisc3D.R
@@ -1,16 +1,69 @@
+twApply3DMesh <- function(
+	### Applying FUN over cube-grid of x,y,z values
+	x,y=NULL,z=NULL				##<< range of x,y and z ordinate see \code{\link{xyz.coords}}
+	,FUN="+", argsFUN=list()	
+	,dims=5						##<< integer vector of length 1 or 3: number of points
+	,knotSpacing=c(				##<< method for calulating the knots. 
+		##describe<<
+		quantile="quantile"		##<< \code{\link{cutQuantiles}} for midpoints of intervals holding about equal number of points (default) 
+		,all="all"				##<< take all the provided xyz coordinates (overwrites nKnots)
+		,equidistant="equidistant")	##<< cover the range of dimension i by \code{dims[i]} equidistant points
+	##end<<
+	,nSample=0			##<< number of points to sample from xyz in addition to grid
+	## Results will be provided in dataframe of four coloumns with attribute "sample".
+	,label=deparse(substitute(FUN))	##<< label of the z-variable, stored in attribute label
+	,...				##<< further arguments passed to FUN
+){
+	##seealso<< \code{\link{plot.twApply3DMesh}}
+	##seealso<< \code{\link{twPairs}}, \link{twMisc}
+	if( length(dims)<3 ) dims <- rep(dims[1],3)
+	xyz <- xyz.coords(x,y,z)
+	if( 0==length(xyz$xlab) ) xyz$xlab=deparse(substitute(x))
+	if( 0==length(xyz$ylab) ) xyz$ylab=deparse(substitute(y))
+	if( 0==length(xyz$zlab) ) xyz$zlab=deparse(substitute(z))
+	dr <- lapply(xyz[1:3],range)
+	#i<-1;mtrace(.calcKnots);.calcKnots(xyz[[i]], nKnots=dims[i], knotSpacing=knotSpacing) 
+	grid <- lapply(1:3, function(i){ twMisc:::.calcKnots(xyz[[i]], nKnots=dims[i], knotSpacing=knotSpacing) })
+	names(grid) <- names(xyz)[1:3]
+	mydf <- do.call( expand.grid, grid )
+	h <- do.call(FUN, c(list(mydf$x,mydf$y,mydf$z),argsFUN,list(...)) )
+	res <- array(h, dim=dims, dimnames=list(x=NULL,y=NULL,z=NULL))
+	names(dimnames(res)) <- names(grid) <- c(xyz$xlab,xyz$ylab,xyz$zlab)
+	# sample points
+	if( 0 < nSample ){
+		ds <- as.data.frame(xyz[1:3])
+		dsSub <- if( nSample < nrow(ds)) ds[ sample.int(nrow(ds),nSample),] else ds
+		dsSub$h <- do.call(FUN, c(list(dsSub$x,dsSub$y,dsSub$z),argsFUN,list(...)) )
+		names(dsSub) <- c(names(dimnames(res)), label)
+	}
+	resList <- list( mesh=do.call(cbind, grid), fval=res, sample=dsSub, label=label, origRange=lapply(xyz[1:3],range))
+	class(resList) <- "twApply3DMesh"
+	resList
+	### list of class twApply2DMesh with itmes \itemize{
+	### \item mesh:matrix with each row one coordinate and two columns corresponding to x and y 
+	### \item fval: the two dimensional array of evaluated function values
+	### \item sample: data frame with four columns corresponding to points additional to the grid
+	### \item label: argument label describing fval
+	### \item rangeOrig: list with items x, and y with the range of the original sample
+	### }
+}
+
+
+
 setMethodS3("plot","twApply3DMesh", function( 
 		### Creates an rgl scene with contours from calculated mesh.
 		x							##<< results of \code{\link{twApply3DMesh}}
 		, probs=NULL 				##<< numeric vector: levels will be calculated with quantiles of the results of the sample
 		, levels=NULL				##<< numeric vector: values of FUN return values where contour surface should be drawn
 		, xlab=NULL, ylab=NULL, zlab=NULL	##<< labels, default to variable names or column names in x
-		, sample=attr(x,"sample")	##<< dataframe of four columns: sample points to draw
+		, sample=x$sample			##<< dataframe of four columns: sample points to draw
 		, nDrawPoints=if( 0<length(sample) ) nrow(sample) else 200
 		### number of points in xyz to draw. Set to 0 if no points should be drawn
 		, col= rev(heat.colors(100))	##<< colour of the points
 		, alo = 0.1, ahi = 0.5		##<< minimum and maximum transparency for contour levels
 		, cmap = heat.colors
 		, box = TRUE, axes = TRUE	##<< whether to draw box and axes
+		,xlim=NULL,ylim=NULL,zlim=NULL		
 		, ...						##<< further arguments to \code{\link{plot3d}}
 	){
 		# plot.twApply3DMesh
@@ -23,11 +76,14 @@ setMethodS3("plot","twApply3DMesh", function(
 		## }
 		##}}
 		
-		dn <- sapply( dimnames(x), as.numeric )
-		xyz <- xyz.coords(dn)
+		xyz <- xyz.coords(x$mesh)
 		if( 0==length(xlab) ) xlab=xyz$xlab
 		if( 0==length(ylab) ) ylab=xyz$ylab
 		if( 0==length(zlab) ) zlab=xyz$zlab
+		
+		if( 0==length(xlim)) xlim=x$rangeOrig$x
+		if( 0==length(ylim)) ylim=x$rangeOrig$y
+		if( 0==length(zlim)) ylim=x$rangeOrig$z
 		
 		##details<<
 		## If argument \code{level} is supplied, the argument \code{probs} is ignored. 
@@ -42,7 +98,7 @@ setMethodS3("plot","twApply3DMesh", function(
 				levels <- quantile(as.vector(sample[,4]),probs=probs)
 		
 		ds <- expand.grid(xyz[1:3])
-		ds$h <- as.vector(x)
+		ds$h <- as.vector(x$fval)
 		
 		# drawing points
 		if( nDrawPoints > 0){
@@ -56,7 +112,8 @@ setMethodS3("plot","twApply3DMesh", function(
 			plot3d(sample
 				, col = col[ round(twRescale(sample[,4],c(1,length(col)))) ]
 				, box = FALSE, axes = FALSE
-			#, ...
+				, xlim=xlim, ylim=ylim, zlim=zlim
+				, ...
 			)
 		}# drawing points
 		
@@ -65,7 +122,8 @@ setMethodS3("plot","twApply3DMesh", function(
 		if( 0 < nc){
 			col <- rev(cmap(length(levels)))
 			al <- seq(alo, ahi, len = length(levels))
-			contour3d(x,levels,xyz$x,xyz$y,xyz$z,color=col,alpha=al, add=( nDrawPoints > 0 ), box = FALSE, axes = FALSE
+			contour3d(x$fval,levels,xyz$x,xyz$y,xyz$z,color=col,alpha=al, add=( nDrawPoints > 0 ), box = FALSE, axes = FALSE
+				, xlim=xlim, ylim=ylim, zlim=zlim
 				, ...
 			)
 		}
@@ -112,49 +170,5 @@ attr(plot.twApply3DMesh,"ex") <- function(){
 }
 
 
-twApply3DMesh <- function(
-	### Applying FUN over cube-grid of x,y,z values
-	x,y=NULL,z=NULL				##<< range of x,y and z ordinate see \code{\link{xyz.coords}}
-	,FUN="+", argsFUN=list()	
-	,dims=5						##<< integer vector of length 1 or 3: number of points
-	,knotSpacing=c(				##<< method for calulating the knots. 
-		##describe<<
-		quantile="quantile"		##<< \code{\link{cutQuantiles}} for midpoints of intervals holding about equal number of points (default) 
-		,all="all"				##<< take all the provided xyz coordinates (overwrites nKnots)
-		,equidistant="equidistant")	##<< cover the range of dimension i by \code{dims[i]} equidistant points
-		##end<<
-	,nSample=0			##<< number of points to sample from xyz in addition to grid
-		## Results will be provided in dataframe of four coloumns with attribute "sample".
-	,label=deparse(substitute(FUN))	##<< label of the z-variable, stored in attribute label
-	,...				##<< further arguments passed to FUN
-){
-	##seealso<< \code{\link{plot.twApply3DMesh}}
-	##seealso<< \code{\link{twPairs}}, \link{twMisc}
-	if( length(dims)<3 ) dims <- rep(dims[1],3)
-	xyz <- xyz.coords(x,y,z)
-	if( 0==length(xyz$xlab) ) xyz$xlab=deparse(substitute(x))
-	if( 0==length(xyz$ylab) ) xyz$ylab=deparse(substitute(y))
-	if( 0==length(xyz$zlab) ) xyz$zlab=deparse(substitute(z))
-	dr <- lapply(xyz[1:3],range)
-	#i<-1;mtrace(.calcKnots);.calcKnots(xyz[[i]], nKnots=dims[i], knotSpacing=knotSpacing) 
-	grid <- lapply(1:3, function(i){ twMisc:::.calcKnots(xyz[[i]], nKnots=dims[i], knotSpacing=knotSpacing) })
-	names(grid) <- names(xyz)[1:3]
-	mydf <- do.call( expand.grid, grid )
-	h <- do.call(FUN, c(list(mydf$x,mydf$y,mydf$z),argsFUN,list(...)) )
-	res <- array(h, dim=dims, dimnames=grid)
-	names(dimnames(res)) <- c(xyz$xlab,xyz$ylab,xyz$zlab)
-	class(res) <- "twApply3DMesh"
-	# sample points
-	if( 0 < nSample ){
-		ds <- as.data.frame(xyz[1:3])
-		dsSub <- if( nSample < nrow(ds)) ds[ sample.int(nrow(ds),nSample),] else ds
-		dsSub$h <- do.call(FUN, c(list(dsSub$x,dsSub$y,dsSub$z),argsFUN,list(...)) )
-		names(dsSub) <- c(names(dimnames(res)), label)
-		attr(res,"sample") <- dsSub
-	}
-	attr(res,"label") <- label
-	res
-	### three dimensional array of class twApply3DMesh with results of calling FUN. Attribute dimnames holds the mesh of respective arguments.
-}
 
 
