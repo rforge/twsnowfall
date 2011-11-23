34c5424933eda393b2fc9b585f834f8d7de4002a
tomaschwutz <tomaschwutz@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-11-23 08:25:48 +0000

    simplified remoteWrapper saving one eval
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/twsnowfall@40 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/twSnowfall/R/remoteWrapper.R
+++ pkg/twSnowfall/R/remoteWrapper.R
@@ -27,17 +27,17 @@ sfRemoteWrapper <- function(
 	if( is.null(remoteFun) && !is.null(remoteFunArgs$remoteFun) ){
 		remoteFun <- remoteFunArgs$remoteFun
 	}
-	remoteFunArgs$remoteFun <- NULL #do not provide argument to fLogLik
+	remoteFunArgs$remoteFun <- NULL #do not provide argument to remoteFun
 	if( is.null(remoteDumpfileBasename) && !is.null(remoteFunArgs$remoteDumpfileBasename)){
 		remoteDumpfileBasename <- remoteFunArgs$remoteDumpfileBasename; 
 	}
-	remoteFunArgs$remoteDumpfileBasename <- NULL	#do not provide argument to fLogLik
+	remoteFunArgs$remoteDumpfileBasename <- NULL	#do not provide argument to remoteFun
 	#if( is.null(remoteFun) stop
 	
 	#dump.frames(remoteDumpfileBasename,TRUE)
 	#stop(paste("dumped frames to",remoteDumpfileBasename) )
 	
-	body <- expression( comp <- do.call(remoteFun, c(remoteFunArgs, list(...)), quote=TRUE) )
+	#body <- expression( comp <- do.call(remoteFun, c(remoteFunArgs, list(...)), quote=TRUE) )
 	##details<<  
 	## If dumpFileBaseName is not null, a dump is created in 
 	## this file when an error occurs.
@@ -46,14 +46,17 @@ sfRemoteWrapper <- function(
 	## \item{\code{mtrace(remoteFun); try( eval(body) )}}
 	## }
 	if( !is.null(remoteDumpfileBasename)){
-		comp <- try( eval(body), silent=TRUE )
+		#comp <- try( eval(body), silent=TRUE )
+		comp <- try( 
+			comp <- do.call(remoteFun, c(remoteFunArgs, list(...)), quote=TRUE)
+			, silent=TRUE )
 		if( inherits(comp, "try-error") ){
 			dump.frames(remoteDumpfileBasename,TRUE)
 			stop(comp)
 		}
 		comp
 	}else{
-		comp <- eval(body)
+		comp <- do.call(remoteFun, c(remoteFunArgs, list(...)), quote=TRUE)
 	}
 	comp
 	### result of calling remoteFun 
